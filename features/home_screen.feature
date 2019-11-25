29bd33bdc076a7f8680a8ead4603633427746c87
Ivan Zhong <ivanzhong0310@gmail.com>
2019-11-25 15:15:35 +1300

    Adding argumnets to steps with paremeter

--- features/home_screen.feature
+++ features/home_screen.feature
@@ -1,6 +1,7 @@
 Feature: Tests for Home screen functionality
   Scenario: Deafult values on Home screen is Foot and Centimeter
     Given I land on home screen
-    Then Left Unit picker value should be Foot
-    And Right Unit picker value should be Centimeter
-    
+    Then Left Unit picker value should be "Foot"
+    Then Left Unit picker value should be "Inch"
+
+    And Right Unit picker value should be "Centimeter"
--- features/step_definitions/home_screen_steps.rb
+++ features/step_definitions/home_screen_steps.rb
@@ -1,7 +1,16 @@
-Then("Left Unit picker value should be Foot") do
-  puts("Left unit picker value is Foot")
+# Then("Left Unit picker value should be Foot") do
+  # puts("Left unit picker value is Foot")
+# end
+
+Then("Left Unit picker value should be {string}") do |string|
+  puts(obj="Left unit picker value is "+string)
+
 end
 
-Then("Right Unit picker value should be Centimeter") do
-  puts("Right unit picker value is Centimeter")
+# Then("Right Unit picker value should be Centimeter") do
+  # puts("Right unit picker value is Centimeter")
+# end
+
+Then("Right Unit picker value should be {string}") do |string|
+  puts("Right unit picker value is " + string)
 end
